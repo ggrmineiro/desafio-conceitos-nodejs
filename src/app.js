df82424218c6e1773f4b68bb69fd442357dc5dae
Gustavo <ggr_mineiro@hotmail.com>
2020-05-12 03:22:10 -0300

    Initial repository

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,77 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  if (repositories.length === 0) {
+    return response.status(200).json({ msg: "No projects found." });
+  }
+
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+  
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0) {
+    return response.status(400).json({ msg: "Repository doesn't exists." });
+  }
+
+  const originalLike = repositories[index].likes;
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: originalLike,
+  };
+  repositories[index] = repository;
+
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0) {
+    return response.status(400).json({ msg: "Repository not found." });
+  }
+  repositories.splice(index, 1);
+  console.log(repositories[index]);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0) {
+    return response.status(400).json({ msg: "Repository not found." });
+  }
+
+  repository = repositories[index];
+  repository.likes = repository.likes + 1;
+
+  return response.status(200).json(repository);
 });
 
 module.exports = app;
